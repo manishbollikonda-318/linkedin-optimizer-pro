0222d4086a320cbccd9e199e5019287e20b01167
manishbollikonda-318 <manishbollikonda318@gmail.com>
2026-04-19 00:41:15 +0530

    🚀 Production Build: Restored Multi-Page State Machine

---
+++ out/_next/static/-3CcBzvYp32qFqgZ4_XUn/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/linkedin-optimizer-pro//_next/:path+",
+        "destination": "/linkedin-optimizer-pro/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
